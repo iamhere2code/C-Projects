dbf0a52708dd347afa374a0ad76f717d25794365
Laila <73257245+iamhere2code@users.noreply.github.com>
2024-09-22 10:12:03 -0700

    Add files via upload
    C++ Lesson One

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    std::cout<<"Hello, welcome to my coding lesson\n";
+    std::cout<<"Hello, how r u?\n";
+    std::cout<<"I am doing good"<<std::endl;
+    std::cout<<"The lesson was fun!";
+    return 0;
+}
