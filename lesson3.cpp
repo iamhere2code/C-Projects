17d72fe46733f10fa03ab6d1218712f8eeb15ba8
Laila <73257245+iamhere2code@users.noreply.github.com>
2024-11-18 22:53:12 -0800

    Create lesson3.cpp

---
+++ lesson3.cpp
@@ -0,0 +1,91 @@
+/*#include <iostream>
+int main() {
+    int x;
+    std::cout<<"Type a number: ";
+    std::cin>>x;
+    std::cout<<"Your number is: "<<x;
+    return 0;
+}*/
+
+//cin example
+/*#include <iostream>
+int main() {
+    int age;
+    std::cout<<"Type in your age here: ";
+    std::cin>>age;
+    std::cout<<"Your age is "<<age;
+    return 0;
+}*/
+
+//addition example
+/*#include <iostream>
+int main() {
+    int x, y;
+    int sum;
+    std::cout<<"Type first number: ";
+    std::cin>>x;
+    std::cout<<"Type second number: ";
+    std::cin>>y;
+    sum = x + y;
+    std::cout<<"Your sum is "<<sum;
+}*/
+
+//product example
+/*#include <iostream>
+int main() {
+    int x, y;
+    int product;
+    std::cout<<"Type first number: ";
+    std::cin>>x;
+    std::cout<<"Type second number: ";
+    std::cin>>y;
+    product = x * y;
+    std::cout<<"Your product is "<<product;
+}*/
+
+//Division example
+/*#include <iostream>
+int main() {
+    float x, y;
+    float division;
+    std::cout<<"Type first number: ";
+    std::cin>>x;
+    std::cout<<"Type second number: ";
+    std::cin>>y;
+    division = x/y;
+    std::cout<<"Your answer is "<<division;
+}*/
+
+//Modulus example
+/*#include <iostream>
+int main(){
+    int x = 10;
+    int y = 3;
+    std::cout<<"The remainder of x/y is "<<x%y;
+}*/
+
+//increment example
+/*#include <iostream>
+int main() {
+    int x = 5;
+    ++x;
+    std::cout<<"When x is incremented it becomes "<<x;
+}*/
+
+//decrement example
+/*#include <iostream>
+int main() {
+    int x = 5;
+    --x;
+    std::cout<<"When x is decremented it becomes "<<x;
+}*/
+
+//assignment operators example
+#include <iostream>
+int main() {
+    int x = 8;
+    x%=3;
+    std::cout<<x;
+    return 0;
+    
+}
