b236d7230a236075cbee8d7735ad3a201b535f9d
Laila <73257245+iamhere2code@users.noreply.github.com>
2024-11-18 22:54:34 -0800

    Create lesson6.cpp

---
+++ lesson6.cpp
@@ -0,0 +1,38 @@
+/*#include <iostream>
+int main() {
+    int x = 10;
+    x >>= 10;
+    std::cout<<x;
+}*/
+
+// Comparison Operators
+
+/*#include <iostream>
+int main() {
+    int x = 10;
+    int y = 5;
+    // Greater/less than or equal too
+    std::cout<<(x != y);
+    return 0;
+}*/
+
+//Assignment Operators
+//&& and Operator
+//|| or Operator
+//! not Operator
+
+/*#include <iostream>
+int main() {
+    int x = 10;
+    int y = 5;
+    std::cout<<(!(x > 11 || y < 4));
+    return 0;
+}*/
+
+//ASCII Operators
+#include <iostream>
+int main(){
+    char a = 30, b =31, c = 32;
+    std::cout<<a<<b<<c;
+    return 0;
+}
